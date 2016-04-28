0ec4af3af5dc017c10b935ea93dfc666c24e293e
Steven Walley <swalley2626@yahoo.com>
2016-04-27 20:58:34 -0700

    Added files via upload

---
+++ Dice.java
@@ -0,0 +1,10 @@
+
+public class Dice {
+	
+	public static int OneThrow()
+	{
+		int randNum = 1+(int)(Math.random()*6);
+		return randNum;
+	}
+
+}
---
+++ DiceTest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DiceTest {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		Dice dice = new Dice();
+		
+		System.out.println(dice.OneThrow());
+		
+
+	}
+
+}
